300c5424699a6e99478a69305ee0e5ee6fa65f81
rame <ochike41@gmail.com>
2023-12-14 09:38:25 +0100

    folder-code1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Components/Home'
+import About from './Components/About'
+// import Work from './Components/Work'
+// import Testimonial from './Components/Testimonial'
+// import Contact from './Components/Contact'
+// import Footer from './Components/Footer'
+
+function App() {
+  
+
+  return (
+    
+      <div className='App'>
+       <Home/>
+        <About/>
+     
+      </div>
+      
+    
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import About from './About';
+// import BannerBackground from "../Assets/home-banner-background.png";
+// import BannerImage from "../Assets/home-banner-image.png";
+// import Navbar from "./Navbar";
+// import { FiArrowRight } from "react-icons/fi";
+const Home = () => {
+  return (
+    <div className="home-container">
+       <Navbar/>
+       
+    </div>
+  );
+};
+
+export default Home
